f014fff93807cacdba910ef1456a612d8338e361
Mr. Hyde <mr.hyde@2bad.me>
2018-09-17 13:58:50 +0300

    [conf] resolve common js modules for `rollup` bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,29 @@
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import pkg from './package.json'
 
 export default [
   {
     plugins: [
+      resolve(),
+      commonjs(),
       babel({
         exclude: 'node_modules/**'
       })
     ],
+    external: [
+      'util', 'tty', 'os'
+    ],
     input: 'source/main.js',
-    'output': [
+    output: [
       {
         file: pkg.main,
         format: 'cjs'
       },
       {
         file: pkg.module,
-        format: 'es'
+        format: 'esm'
       }
     ]
   }
