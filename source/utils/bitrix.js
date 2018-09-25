96982837469a474ed4a558ef37f3212e7514947f
Mr. Hyde <mr.hyde@2bad.me>
2018-09-25 20:31:12 +0300

    [js] reorganize and comment bitrix calls

--- source/utils/bitrix.js
+++ source/utils/bitrix.js
@@ -1,4 +1,5 @@
 import got from 'got'
+import fromEntries from './type/fromEntries.js'
 import { version } from './../../package.json'
 
 const TOKEN = ''
@@ -20,18 +21,18 @@ const client = got.extend({
   }
 })
 
-export const getTotal = async (resource) => {
-  return client.get(resource).then(response => response.body.total)
-}
+export const getTotal = async (resource) => client.get(resource).then(response => response.body.total)
 
-export const getData = async (resource) => {
-  const total = await getTotal(resource)
+export const getDataByPage = async (resource, total) => {
   const requests = []
 
+  // generate array of requests
   for (let i = 0; i <= total; i += 50) {
     requests.push(client.get(resource, { query: { start: i } }).then(response => response.body.result))
   }
 
-  // flatten data to single level array
-  return Promise.all(requests).then(data => [].concat(...data))
+  // wait for all requests to finish and return flatten data to single level array
+  return Promise.all(requests).then(data => Array.concat(...data))
+}
+}
 }
