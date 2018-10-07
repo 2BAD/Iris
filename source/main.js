be8061c980c740b4218b8de36f16e2e820f063db
Mr. Hyde <mr.hyde@2bad.me>
2018-10-08 00:55:04 +0300

    [js] create instance of Bitrix class using environment variables

--- source/main.js
+++ source/main.js
@@ -1,6 +1,11 @@
-import { fetch } from './utils/bitrix.js'
+import Bitrix from './utils/bitrix.js'
 import saveToFile from './utils/fs/saveToFile.js'
 
-fetch('crm.deal.list')
+const REST_URI = process.env.REST_URI
+const TOKEN = process.env.TOKEN
+
+const bx = new Bitrix(REST_URI, TOKEN)
+
+bx.fetch('crm.deal.list')
   .then(data => saveToFile('deals.json', data))
   .catch(error => console.log(error))
