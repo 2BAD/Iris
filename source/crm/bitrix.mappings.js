bd204946d151aef919adf7a38e35bd33c4b1e779
Mr. Hyde <mr.hyde@2bad.me>
2018-10-16 23:16:54 +0300

    [js] add methods and filename mappings for bitrix crm

---
+++ source/crm/bitrix.mappings.js
@@ -0,0 +1,30 @@
+export const resources = [
+  {
+    method: 'crm.deal.list',
+    filename: 'data/deals.json'
+  },
+  {
+    method: 'crm.lead.list',
+    filename: 'data/leads.json'
+  },
+  {
+    method: 'crm.company.list',
+    filename: 'data/companies.json'
+  },
+  {
+    method: 'crm.contact.list',
+    filename: 'data/contacts.json'
+  },
+  {
+    method: 'crm.product.list',
+    filename: 'data/products.json'
+  },
+  {
+    method: 'crm.status.list',
+    filename: 'data/statuses.json'
+  },
+  {
+    method: 'user.get',
+    filename: 'data/users.json'
+  }
+]
