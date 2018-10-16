c025e9d6ede4076d79113084accb693014399e0c
Mr. Hyde <mr.hyde@2bad.me>
2018-10-17 00:21:27 +0300

    [js] add name property to bitrix mapping

--- source/crm/bitrix.mappings.js
+++ source/crm/bitrix.mappings.js
@@ -1,29 +1,36 @@
 const resources = [
   {
+    name: 'deals',
     method: 'crm.deal.list',
     filename: 'data/deals.json'
   },
   {
+    name: 'leads',
     method: 'crm.lead.list',
     filename: 'data/leads.json'
   },
   {
+    name: 'companies',
     method: 'crm.company.list',
     filename: 'data/companies.json'
   },
   {
+    name: 'contacts',
     method: 'crm.contact.list',
     filename: 'data/contacts.json'
   },
   {
+    name: 'products',
     method: 'crm.product.list',
     filename: 'data/products.json'
   },
   {
+    name: 'statuses',
     method: 'crm.status.list',
     filename: 'data/statuses.json'
   },
   {
+    name: 'users',
     method: 'user.get',
     filename: 'data/users.json'
   }
