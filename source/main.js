62a393af259fa8b558bc8606471ff6379cbd441d
Mr. Hyde <mr.hyde@2bad.me>
2018-10-16 23:18:34 +0300

    [js] fetch and save multiple resources at once

--- source/main.js
+++ source/main.js
@@ -1,6 +1,7 @@
 import Bitrix from '@2bad/iris.crm.bitrix'
 import { save } from './s3.js'
 import { success, failure } from './utils/response.js'
+import { resources } from './crm/bitrix.mappings.js'
 
 export const main = async (event, context) => {
   const REST_URI = event.queryStringParameters.REST_URI
@@ -8,9 +9,15 @@ export const main = async (event, context) => {
 
   try {
     const bitrix = new Bitrix(REST_URI, TOKEN)
-    const result = await bitrix.fetch('user.get')
 
-    await save(result, 'user.json')
+    await Promise.all(resources.map(async resource => {
+      console.time(`awaiting ${resource.method}`)
+      const result = await bitrix.fetch(resource.method)
+      console.timeEnd(`awaiting ${resource.method}`)
+      console.time(`saving ${resource.filename} (${result.length} entries)`)
+      await save(result, resource.filename)
+      console.timeEnd(`saving ${resource.filename} (${result.length} entries)`)
+    }))
 
     return success('file saved')
   } catch (e) {
