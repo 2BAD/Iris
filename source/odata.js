ae6fbb82ec2c1248471fa47b89c17f9b921ab7e0
Mr. Hyde <mr.hyde@2bad.me>
2018-10-19 11:57:39 +0300

    [js] implement odata wrapper function

--- source/odata.js
+++ source/odata.js
@@ -1,8 +1,22 @@
-const wrap = (data) => {
+const PAGE_SIZE = 500
+
+const wrap = (data, { queryStringParameters, pathParameters }) => {
+  const skip = (queryStringParameters && queryStringParameters.$skip) ? parseInt(queryStringParameters.$skip) : 0
+  const { resource } = pathParameters || {}
+  const link = 'https://botjva5j5i.execute-api.eu-central-1.amazonaws.com/development/iris/odata/'
+  const metadataLink = `${link}$metadata`
+  const context = (resource) ? `${metadataLink}#${resource}` : metadataLink
+  const offset = PAGE_SIZE + skip
+
   const response = {
-    '@odata.context': '',
-    value: data
+    '@odata.context': context,
+    'value': data.slice(skip, offset)
   }
+
+  if (resource && data.length > offset) {
+    response['@odata.nextLink'] = `${link}${resource}?$skip=${offset}`
+  }
+
   return response
 }
 
